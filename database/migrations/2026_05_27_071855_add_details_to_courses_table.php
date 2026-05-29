03b19c6a0fc719da89d26f972839f93a7f3ed975
IDCDEV03 <123153213+IDCDEV03@users.noreply.github.com>
2026-05-30 00:31:50 +0700

    Update 2026_05_27_071855_add_details_to_courses_table.php

--- database/migrations/2026_05_27_071855_add_details_to_courses_table.php
+++ database/migrations/2026_05_27_071855_add_details_to_courses_table.php
@@ -7,14 +7,19 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
 public function up()
-    {
-        Schema::table('courses', function (Blueprint $table) {
-            // เพิ่มคอลัมน์ใหม่ต่อท้าย
+{
+    Schema::table('courses', function (Blueprint $table) {
+        if (!Schema::hasColumn('courses', 'description')) {
             $table->text('description')->nullable()->after('course_name');
+        }
+        if (!Schema::hasColumn('courses', 'default_price')) {
             $table->decimal('default_price', 10, 2)->nullable()->after('description');
+        }
+        if (!Schema::hasColumn('courses', 'is_active')) {
             $table->boolean('is_active')->default(true)->after('default_price');
-        });
-    }
+        }
+    });
+}
 
     public function down()
     {
